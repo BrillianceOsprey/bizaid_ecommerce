12d1ca80725b46bcaa78f9c52e0041d046c4f91d
MyatSoe <mssoelay91@gmail.com>
2023-12-02 22:00:35 +0630

    add ui and model

--- lib/src/features/home/presentation/home_page.dart
+++ lib/src/features/home/presentation/home_page.dart
@@ -1,7 +1,10 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:starter_app/src/features/category/presentation/category_page.dart';
 import 'package:starter_app/src/features/home/presentation/widgets/home_product_grid_view.dart';
+import 'package:starter_app/src/shared/utils/extensions/media_query_extension.dart';
+import 'package:starter_app/src/shared/utils/flutter_extension.dart';
 
 class HomePage extends ConsumerStatefulWidget {
   const HomePage({super.key});
@@ -27,7 +30,7 @@ class HomePageState extends ConsumerState<HomePage> {
           ),
 
           SizedBox(
-            height: MediaQuery.of(context).size.height * 0.14,
+            height: MediaQuery.of(context).size.height * 0.06,
             child: ListView.builder(
               scrollDirection: Axis.horizontal,
               itemCount: 10,
@@ -41,10 +44,13 @@ class HomePageState extends ConsumerState<HomePage> {
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(5),
                   ),
-                  child: const Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 8, vertical: 10),
-                    child: Text(
-                      'First Category',
+                  child: Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8),
+                      child: Text(
+                        'First Category',
+                        style: context.textTheme.titleSmall,
+                      ),
                     ),
                   ),
                 ),
@@ -63,7 +69,8 @@ class HomePageState extends ConsumerState<HomePage> {
                   style: Theme.of(context).textTheme.titleLarge,
                 ),
                 SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.14,
+                  // height: MediaQuery.of(context).size.height * 0.14,
+                  height: 100,
                   child: ListView.builder(
                     scrollDirection: Axis.horizontal,
                     itemCount: 10,
@@ -80,12 +87,46 @@ class HomePageState extends ConsumerState<HomePage> {
                         shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(5),
                         ),
-                        child: const Padding(
-                          padding:
-                              EdgeInsets.symmetric(horizontal: 8, vertical: 10),
-                          child: Text(
-                            'First Category',
-                          ),
+                        child: Column(
+                          children: [
+                            // CachedNetworkImage(
+                            //   height: 70,
+                            //   width: 70,
+                            //   imageUrl:
+                            //       'http://13.228.29.1/productAllImage/HHPSA006.jpg',
+                            //   imageBuilder: (context, imageProvider) =>
+                            //       Container(
+                            //     decoration: BoxDecoration(
+                            //       image: DecorationImage(
+                            //         image: imageProvider,
+                            //         fit: BoxFit.cover,
+                            //         colorFilter: const ColorFilter.mode(
+                            //           Colors.red,
+                            //           BlendMode.colorBurn,
+                            //         ),
+                            //       ),
+                            //     ),
+                            //   ),
+                            //   placeholder: (context, url) =>
+                            //       const CircularProgressIndicator(),
+                            //   errorWidget: (context, url, error) =>
+                            //       const Icon(Icons.error),
+                            // ),
+                            Container(
+                              height: 70,
+                              width: 70,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(5),
+                                image: const DecorationImage(
+                                  image: NetworkImage(
+                                    'http://13.228.29.1/productAllImage/SHATDX003.jpg',
+                                  ),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            const Text('Hanger'),
+                          ],
                         ),
                       ),
                     ),
